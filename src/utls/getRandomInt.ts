71b09becd139f5a304f14dfb5dc6e87b0de9d13d
Doni <doniyorbek.rajabov@zoftify.com>
2022-10-25 20:54:34 +0500

    change #2

--- src/utls/getRandomInt.ts
+++ src/utls/getRandomInt.ts
@@ -1,5 +1,5 @@
 export const getRandomInt = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + max;
 };
 
 export const testFunction = () => {
